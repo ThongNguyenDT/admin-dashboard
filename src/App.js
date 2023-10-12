acfd17b2cefb3782e4e6fb166778c3b5c6967162
ThongNguyenDT <nguyenduongtienthong@gmail.com>
2023-10-12 21:53:46 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { FiSettings } from 'react-icons/fi';
+
+
 import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <BrowserRouter>
+        <div className="flex relative dark:bg-main-dark-bg">
+          <div className='fixed right-4 bottom-4' style={{zIndex: '1000'}}>
+            
+          </div>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
